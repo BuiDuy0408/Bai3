0518bd330e2cf1af61e4e72733551867d2685246
LuThaiHoc <50101866+LuThaiHoc@users.noreply.github.com>
2019-11-18 22:17:58 +0700

    hoc-add file huong dan

--- QuanLyThuVien/QuanLyThuVien/HuongDan.cs
+++ QuanLyThuVien/QuanLyThuVien/HuongDan.cs
@@ -16,5 +16,57 @@ namespace QuanLyThuVien
         {
             InitializeComponent();
         }
+        private void GetFileAll(string tenfile)
+        {
+            StreamReader doc = File.OpenText(tenfile);
+            string s = doc.ReadToEnd();
+            txtGioiThieu.Text = s;
+        }
+
+        private void trViewGioiThieu_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Name == "gtPhanMem")
+            {
+                GetFileAll("GioiThieuChung.txt");
+                Image img = Image.FromFile(@"thu vien.jpg");
+                pictureBox1.BackgroundImage = img;
+            }
+            else if (e.Node.Name == "gtDangNhap")
+            {
+                GetFileAll("PhanDangNhap.txt");
+                Image img = Image.FromFile(@"b3 dang nhap.png");
+                pictureBox1.BackgroundImage = img;
+            }
+            else if (e.Node.Name == "gtManHinhChinh")
+            {
+                GetFileAll("PhanMain.txt");
+                Image img = Image.FromFile(@"b3 main.png");
+                pictureBox1.BackgroundImage = img;
+            }
+            else if (e.Node.Name == "gtDocGia")
+            {
+                GetFileAll("PhanDocGia.txt");
+                Image img = Image.FromFile(@"b3 DocGia.png");
+                pictureBox1.BackgroundImage = img;
+            }
+            else if (e.Node.Name == "gtMuonSach")
+            {
+                GetFileAll("PhanMuonSach.txt");
+                Image img = Image.FromFile(@"b3 MuonSach.png");
+                pictureBox1.BackgroundImage = img;
+            }
+            else if (e.Node.Name == "gtTaiLieu")
+            {
+                GetFileAll("PhanTaiLieu.txt");
+                Image img = Image.FromFile(@"b3 TaiLieu.png");
+                pictureBox1.BackgroundImage = img;
+            }
+            else if (e.Node.Name == "gtTraSach")
+            {
+                GetFileAll("PhanTraSach.txt");
+                Image img = Image.FromFile(@"b3 TraSach.png");
+                pictureBox1.BackgroundImage = img;
+            }
+        }
     }
 }
