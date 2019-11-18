4152899e19ca72655b7073f952aee30decfd3365
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-11-18 15:01:30 +0700

    thanh-add xu li code

--- QuanLyThuVien/QuanLyThuVien/QLThuVien.cs
+++ QuanLyThuVien/QuanLyThuVien/QLThuVien.cs
@@ -16,5 +16,42 @@ namespace QuanLyThuVien
         {
             InitializeComponent();
         }
+
+        private void tàiLiệuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TAI_LIEU frm = new TAI_LIEU();
+            frm.Show();
+
+        }
+
+        private void bạnĐọcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DocGia frm = new DocGia();
+            frm.Show();
+        }
+
+        private void mượnTàiLiệuToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MuonSach frm = new MuonSach();
+            frm.Show();
+        }
+
+        private void thoátToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc muốn thoát?", "Question", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                this.Close();
+        }
+
+        private void hướngDẫnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            HuongDan frm = new HuongDan();
+            frm.Show();
+        }
+
+        private void trảTàiLiệuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmTraSach frm = new frmTraSach();
+            frm.Show();
+        }
     }
 }
