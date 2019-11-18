5644efbf2849bf61b2fb6fe4cbc2c82c2aa87770
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-11-18 14:58:22 +0700

    thanh-insert image to qlthuvien

--- QuanLyThuVien/QuanLyThuVien/QLThuVien.Designer.cs
+++ QuanLyThuVien/QuanLyThuVien/QLThuVien.Designer.cs
@@ -28,11 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QLThuVien));
+            this.picBackground = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBackground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBackground
+            // 
+            this.picBackground.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBackground.BackgroundImage")));
+            this.picBackground.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picBackground.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picBackground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picBackground.Location = new System.Drawing.Point(0, 0);
+            this.picBackground.Name = "picBackground";
+            this.picBackground.Size = new System.Drawing.Size(755, 356);
+            this.picBackground.TabIndex = 2;
+            this.picBackground.TabStop = false;
+            // 
+            // QLThuVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 356);
+            this.Controls.Add(this.picBackground);
+            this.Name = "QLThuVien";
             this.Text = "QLThuVien";
+            ((System.ComponentModel.ISupportInitialize)(this.picBackground)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picBackground;
     }
 }
