4a6f7968861787c30f8a42a5f15d08dca89deb64
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-11-18 13:38:56 +0700

    thanh-add button,textbox

--- QuanLyThuVien/QuanLyThuVien/MuonSach.Designer.cs
+++ QuanLyThuVien/QuanLyThuVien/MuonSach.Designer.cs
@@ -28,11 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblMaBD = new System.Windows.Forms.Label();
+            this.txtMaBD = new System.Windows.Forms.TextBox();
+            this.lblMaTL = new System.Windows.Forms.Label();
+            this.txtMaTL = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblMaBD
+            // 
+            this.lblMaBD.AutoSize = true;
+            this.lblMaBD.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaBD.Location = new System.Drawing.Point(28, 36);
+            this.lblMaBD.Name = "lblMaBD";
+            this.lblMaBD.Size = new System.Drawing.Size(135, 19);
+            this.lblMaBD.TabIndex = 1;
+            this.lblMaBD.Text = "Nhập Mã Bạn Đọc";
+            // 
+            // txtMaBD
+            // 
+            this.txtMaBD.Location = new System.Drawing.Point(169, 37);
+            this.txtMaBD.Name = "txtMaBD";
+            this.txtMaBD.Size = new System.Drawing.Size(164, 20);
+            this.txtMaBD.TabIndex = 2;
+            // 
+            // lblMaTL
+            // 
+            this.lblMaTL.AutoSize = true;
+            this.lblMaTL.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaTL.Location = new System.Drawing.Point(357, 41);
+            this.lblMaTL.Name = "lblMaTL";
+            this.lblMaTL.Size = new System.Drawing.Size(130, 16);
+            this.lblMaTL.TabIndex = 3;
+            this.lblMaTL.Text = "Nhập Mã Tài Liệu";
+            // 
+            // txtMaTL
+            // 
+            this.txtMaTL.Location = new System.Drawing.Point(510, 40);
+            this.txtMaTL.Name = "txtMaTL";
+            this.txtMaTL.Size = new System.Drawing.Size(173, 20);
+            this.txtMaTL.TabIndex = 4;
+            // 
+            // MuonSach
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 261);
+            this.Controls.Add(this.txtMaTL);
+            this.Controls.Add(this.lblMaTL);
+            this.Controls.Add(this.txtMaBD);
+            this.Controls.Add(this.lblMaBD);
+            this.Name = "MuonSach";
             this.Text = "MuonSach";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblMaBD;
+        private System.Windows.Forms.TextBox txtMaBD;
+        private System.Windows.Forms.Label lblMaTL;
+        private System.Windows.Forms.TextBox txtMaTL;
     }
 }
