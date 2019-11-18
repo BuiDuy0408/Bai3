93f5e3345f0a41c969be2962751fdd3c43b02bfa
LuThaiHoc <50101866+LuThaiHoc@users.noreply.github.com>
2019-11-18 22:19:11 +0700

    hoc add code for dang nhap

--- QuanLyThuVien/QuanLyThuVien/frmDangNhap.cs
+++ QuanLyThuVien/QuanLyThuVien/frmDangNhap.cs
@@ -16,5 +16,26 @@ namespace QuanLyThuVien
         {
             InitializeComponent();
         }
+        BUS_DangNhap dn = new BUS_DangNhap();
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            if (dn.DangNhap(txtUserName.Text, txtPass.Text) == true)
+            {
+                // bool x = false;
+                MessageBox.Show("Bạn đăng nhập thành công ^^", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                frmQLThuVien frm1 = new frmQLThuVien();
+                frm1.Show();
+                Hide();
+            }
+            else MessageBox.Show("Tên đăng nhập hoặc mật khẩu sai. Mời bạn nhập lại !!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Ban có chắc muốn thoát ??", "Question", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                this.Close();
+        }
+
     }
 }
