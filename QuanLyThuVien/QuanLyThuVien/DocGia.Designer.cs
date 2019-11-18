386c5284cd872d26530a664ae7aa1af58b801db8
truongmta190298 <camamidol@gmail.com>
2019-11-18 22:51:58 +0700

    ducturong editdesign

--- QuanLyThuVien/QuanLyThuVien/DocGia.Designer.cs
+++ QuanLyThuVien/QuanLyThuVien/DocGia.Designer.cs
@@ -332,10 +332,10 @@
             // 
             // dataGridView1
             // 
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(-5, 279);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(1183, 202);
             this.dataGridView1.TabIndex = 39;
