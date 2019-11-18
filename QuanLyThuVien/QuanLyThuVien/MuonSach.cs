b0937c8e352b5eb9f2c313b07ad3bdde52d9bcf6
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-11-18 14:52:29 +0700

    thanh-add xu li click

--- QuanLyThuVien/QuanLyThuVien/MuonSach.cs
+++ QuanLyThuVien/QuanLyThuVien/MuonSach.cs
@@ -62,5 +62,59 @@ namespace QuanLyThuVien
                 }
             }
         }
+        public void Show(DataGridView dgv)
+        {
+            int i = 0;
+            dgv.RowCount = listTLMuon.Count;
+            for (i = 0; i < listTLMuon.Count; i++)
+            {
+                dgv.Rows[i].Cells[0].Value = listTLMuon[i].Matl.ToString();
+                dgv.Rows[i].Cells[1].Value = listTLMuon[i].Nhande.ToString();
+                dgv.Rows[i].Cells[2].Value = listTLMuon[i].Tacgia.ToString();
+                dgv.Rows[i].Cells[3].Value = listTLMuon[i].Soluong.ToString();
+                dgv.Rows[i].Cells[4].Value = listTLMuon[i].Domat.ToString();
+                dgv.Rows[i].Cells[5].Value = listTLMuon[i].Ngonngu.ToString();
+                dgv.Rows[i].Cells[6].Value = listTLMuon[i].MaTheLoai.ToString();
+                dgv.Rows[i].Cells[7].Value = listTLMuon[i].MaNXB.ToString();
+            }
+        }
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            string MaPM = "";
+            DataTable dt = new DataTable();
+            dt = pm.ThemPhieuMuon(txtMaBD.Text);
+            MaPM = dt.Rows[0]["MaPM"].ToString();
+
+            for (int i = 0; i < dgvSachMuon.Rows.Count; i++)
+            {
+                dt = tl.TimKiemMaSachTheoMaDauSach(dgvSachMuon.Rows[i].Cells[0].Value.ToString());
+                string MaSach = dt.Rows[0]["MaSach"].ToString();
+                bool b = ctpm.ThemCTPM(MaPM, MaSach, DateTime.Now, DateTime.Now.AddMonths(6), "");
+                if (b == false)
+                    MessageBox.Show("Thêm Thất Bại Cuốn :" + dgvSachMuon.Rows[i].Cells[0].Value.ToString());
+                tl.UodateSoLuongDauSachID(dgvSachMuon.Rows[i].Cells[0].Value.ToString());
+            }
+
+            if (MessageBox.Show("Mượn Thành công! Có Muốn kết thúc??", "Question", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                this.Close();
+            else
+            {
+                dgvSachDaMuon.DataSource = bd.ThongKeSachDaMuonTheoID(txtMaBD.Text);
+            }
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            txtMaBD.Text = txtMaTL.Text = "";
+            for (int i = 0; i < dgvSachDaMuon.Rows.Count; i++)
+                dgvSachDaMuon.Rows.RemoveAt(i);
+            for (int i = 0; i < dgvSachMuon.Rows.Count; i++)
+                dgvSachMuon.Rows.RemoveAt(i);
+        }
+
+        private void txtMaBD_TextChanged(object sender, EventArgs e)
+        {
+            dgvSachDaMuon.DataSource = bd.ThongKeSachDaMuonTheoID(txtMaBD.Text);
+        }
     }
 }
