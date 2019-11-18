d9eb227d7fed18bd7195241661fb981142dc7378
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-11-18 13:45:42 +0700

    thanh-add box sach vua nhap

--- QuanLyThuVien/QuanLyThuVien/MuonSach.Designer.cs
+++ QuanLyThuVien/QuanLyThuVien/MuonSach.Designer.cs
@@ -36,7 +36,19 @@
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.btnHuy = new System.Windows.Forms.Button();
             this.btnOK = new System.Windows.Forms.Button();
+            this.gbSachMuon = new System.Windows.Forms.GroupBox();
+            this.dgvSachMuon = new System.Windows.Forms.DataGridView();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox2.SuspendLayout();
+            this.gbSachMuon.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSachMuon)).BeginInit();
             this.SuspendLayout();
             // 
             // lblMaBD
@@ -114,11 +126,92 @@
             this.btnOK.Text = "Hoàn Tất Mượn";
             this.btnOK.UseVisualStyleBackColor = true;
             // 
+            // gbSachMuon
+            // 
+            this.gbSachMuon.Controls.Add(this.dgvSachMuon);
+            this.gbSachMuon.Dock = System.Windows.Forms.DockStyle.Right;
+            this.gbSachMuon.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbSachMuon.Location = new System.Drawing.Point(258, 0);
+            this.gbSachMuon.Name = "gbSachMuon";
+            this.gbSachMuon.Size = new System.Drawing.Size(496, 430);
+            this.gbSachMuon.TabIndex = 7;
+            this.gbSachMuon.TabStop = false;
+            this.gbSachMuon.Text = "Sách Vừa Nhập";
+            // 
+            // dgvSachMuon
+            // 
+            this.dgvSachMuon.AllowUserToAddRows = false;
+            this.dgvSachMuon.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.dgvSachMuon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSachMuon.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12,
+            this.Column13,
+            this.Column14});
+            this.dgvSachMuon.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvSachMuon.Location = new System.Drawing.Point(3, 22);
+            this.dgvSachMuon.Name = "dgvSachMuon";
+            this.dgvSachMuon.Size = new System.Drawing.Size(490, 405);
+            this.dgvSachMuon.TabIndex = 0;
+            // 
+            // Column7
+            // 
+            this.Column7.DataPropertyName = "MaDauSach";
+            this.Column7.HeaderText = "Mã Đầu Sách";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.DataPropertyName = "NhanDe";
+            this.Column8.HeaderText = "Nhan Đề";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.DataPropertyName = "TacGia";
+            this.Column9.HeaderText = "Tác Giả";
+            this.Column9.Name = "Column9";
+            // 
+            // Column10
+            // 
+            this.Column10.DataPropertyName = "SoLuong";
+            this.Column10.HeaderText = "Số Lượng";
+            this.Column10.Name = "Column10";
+            // 
+            // Column11
+            // 
+            this.Column11.DataPropertyName = "DoMat";
+            this.Column11.HeaderText = "Độ Mật";
+            this.Column11.Name = "Column11";
+            // 
+            // Column12
+            // 
+            this.Column12.DataPropertyName = "NgonNgu";
+            this.Column12.HeaderText = "Ngôn Ngữ";
+            this.Column12.Name = "Column12";
+            // 
+            // Column13
+            // 
+            this.Column13.DataPropertyName = "MaTL";
+            this.Column13.HeaderText = "Thể Loại";
+            this.Column13.Name = "Column13";
+            // 
+            // Column14
+            // 
+            this.Column14.DataPropertyName = "MaNXB";
+            this.Column14.HeaderText = "Mã NXB";
+            this.Column14.Name = "Column14";
+            // 
             // MuonSach
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(754, 430);
+            this.Controls.Add(this.gbSachMuon);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.btnMuon);
             this.Controls.Add(this.txtMaTL);
@@ -128,6 +221,8 @@
             this.Name = "MuonSach";
             this.Text = "MuonSach";
             this.groupBox2.ResumeLayout(false);
+            this.gbSachMuon.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSachMuon)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -143,5 +238,15 @@
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button btnHuy;
         private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.GroupBox gbSachMuon;
+        private System.Windows.Forms.DataGridView dgvSachMuon;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
     }
 }
